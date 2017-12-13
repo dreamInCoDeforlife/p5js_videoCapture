2a712a7bd53a16787d8620a727f983360caaa79c
Aman Adhav <amanadhav@gmail.com>
2017-12-13 17:04:38 -0500

    Update video.js

--- video.js
+++ video.js
@@ -1,12 +1,15 @@
+var capture;
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(390, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  //capture.hide();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  background(255);
+  image(capture, 0, 0, 320, 240);
+  filter('INVERT');
 }
+
